06c654ec6167d90bb3ff013958955b3a65179873
Detkovskiy <9119113142@mail.ru>
2016-09-29 15:17:26 +0400

    Сделал основное меню

--- js/map.js
+++ js/map.js
@@ -24,15 +24,28 @@ function init(){
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var userMenu = document.querySelector('.user-menu');
+var formSearch = document.querySelector('.form-search');
+var searchToggle = document.querySelector('.user-menu__link--search');
+
+function changeMenuClass(menuClass, closeSelector, openSelector){
+  if (menuClass.classList.contains(closeSelector)) {
+    menuClass.classList.remove(closeSelector);
+    menuClass.classList.add(openSelector);
+  } else {
+    menuClass.classList.add(closeSelector);
+    menuClass.classList.remove(openSelector);
+  }
+}
 
 navMain.classList.remove('main-nav--nojs');
+userMenu.classList.remove('user-menu--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+  changeMenuClass(navMain, 'main-nav--closed', 'main-nav--opened')
+  changeMenuClass(userMenu, 'user-menu--closed', 'user-menu--opened')
+});
+
+searchToggle.addEventListener('click', function() {
+  changeMenuClass(formSearch, 'form-search--closed', 'form-search--opened')
 });
