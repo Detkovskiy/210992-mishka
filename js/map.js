f8ea8e19fbb89f1739d39d9b82cb085b8eab1b6a
Detkovskiy <9119113142@MAIL.RU>
2016-10-08 03:10:39 +0300

    Сделал графику

--- js/map.js
+++ js/map.js
@@ -27,6 +27,9 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var userMenu = document.querySelector('.user-menu');
 var formSearch = document.querySelector('.form-search');
 var searchToggle = document.querySelector('.user-menu__link--search');
+var link = document.querySelector(".btn");
+var popup = document.querySelector(".modal-add-cart");
+var overlay = document.querySelector(".modal-overlay");
 
 function changeMenuClass(menuClass, closeSelector, openSelector){
   if (menuClass.classList.contains(closeSelector)) {
@@ -48,4 +51,22 @@ navToggle.addEventListener('click', function() {
 
 searchToggle.addEventListener('click', function() {
   changeMenuClass(formSearch, 'form-search--closed', 'form-search--opened')
+});
+
+link.addEventListener("click", function() {
+  popup.classList.add("modal-add-cart-show");
+  overlay.classList.add("modal-overlay-show");
+});
+
+function buy() {
+  if (popup.classList.contains("modal-add-cart-show")) {
+      popup.classList.remove("modal-add-cart-show");}
+  if (overlay.classList.contains("modal-overlay-show")) {
+      overlay.classList.remove("modal-overlay-show");
+}}
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    buy();
+    }
 });
